0cb409b278b08586d4e6af83465c9bc43335d47c
nvhai248 <nvhai2408@gmail.com>
2025-04-13 11:10:15 +0700

    use env

--- lib/utils/app_config.dart
+++ lib/utils/app_config.dart
@@ -1,8 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class AppConfig {
-  static const String supabaseUrl = 'https://sdojefhjykknjyykqqla.supabase.co';
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkb2plZmhqeWtrbmp5eWtxcWxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3MDY0ODMsImV4cCI6MjA1NzI4MjQ4M30.3NXfd12QMdtpK-R5PuAjvnj4Sq1O9_8F8PjZmIeYffI';
-  
-  // Deep linking
-  static const String scheme = 'io.supabase.flutterstarter';
-  static const String host = 'login-callback';
-} 
+  static String get supabaseUrl => dotenv.env['SUPABASE_URL'] ?? '';
+  static String get supabaseAnonKey => dotenv.env['SUPABASE_ANON_KEY'] ?? '';
+  static String get scheme => dotenv.env['SCHEME'] ?? '';
+  static String get host => dotenv.env['HOST'] ?? '';
+}
